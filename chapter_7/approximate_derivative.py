2473e5800ea0087d6eaec5310458f28602182597
Fredrik Nilsson <himske@gmail.com>
2020-12-28 11:26:01 +0100

    fix whitespace and formatting for readability

--- chapter_7/approximate_derivative.py
+++ chapter_7/approximate_derivative.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def f(x):
-    return 2*x**2
+    return 2 * x**2
 
 
 def approximate_tangent_line(x, approximate_derivative, b):
-    return (approximate_derivative*x) + b
+    return (approximate_derivative * x) + b
 
 
 # np.arange(start, stop, step) to give us a smoother curve
@@ -21,23 +21,21 @@ colors = ['k', 'g', 'r', 'b', 'c']
 for i in range(5):
     p2_delta = 0.0001
     x1 = i
-    x2 = x1+p2_delta
+    x2 = x1 + p2_delta
     y1 = f(x1)
     y2 = f(x2)
 
     print((x1, y1), (x2, y2))
 
-    approximate_derivative = (y2-y1)/(x2-x1)
+    approximate_derivative = (y2 - y1) / (x2 - x1)
     b = y2-(approximate_derivative*x2)
-    to_plot = [x1-0.9, x1, x1+0.9]
+    to_plot = [x1 - 0.9, x1, x1 + 0.9]
 
     plt.scatter(x1, y1, c=colors[i])
     plt.plot([point for point in to_plot],
-             [approximate_tangent_line(point, approximate_derivative, b)
-              for point in to_plot],
+             [approximate_tangent_line(point, approximate_derivative, b) for point in to_plot],
              c=colors[i])
 
-    print('Approximate derivative for f(x)',
-          f'where x = {x1} is {approximate_derivative}')
+    print(f'Approximate derivative for f(x) where x = {x1} is {approximate_derivative}')
 
 plt.show()
