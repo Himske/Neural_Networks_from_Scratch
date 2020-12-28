4c210b6bbf751f55ff8223b47518e6438e9c350c
Fredrik Nilsson <himske@gmail.com>
2020-12-28 11:26:42 +0100

    add more whitespace

--- chapter_7/approximate_derivative.py
+++ chapter_7/approximate_derivative.py
@@ -28,7 +28,7 @@ for i in range(5):
     print((x1, y1), (x2, y2))
 
     approximate_derivative = (y2 - y1) / (x2 - x1)
-    b = y2-(approximate_derivative*x2)
+    b = y2 - (approximate_derivative * x2)
     to_plot = [x1 - 0.9, x1, x1 + 0.9]
 
     plt.scatter(x1, y1, c=colors[i])
